5aa5cd65a5f0352bbf81cd2cb1dc46052d80e30f
William Zeng <williamzeng368@gmail.com>
2026-05-22 14:50:42 -0400

    transpose, scale

--- src/math.rs
+++ src/math.rs
@@ -4,8 +4,8 @@ use std::ops::{Deref, DerefMut};
  * todo:
     [x] left identity
     [x] right identity
-    [ ] transpose
-    [ ] scale
+    [x] transpose
+    [x] scale
     [ ] Add (<M, N> + <M, N>)
     [ ] Sub (<M, N> - <M, N>)
     [ ] Mul (<M, N> * <N, O>)
@@ -18,28 +18,72 @@ use std::ops::{Deref, DerefMut};
         [ ] diagonal
  */
 
+#[derive(Debug, Clone)]
 pub struct Matrix<const M: usize, const N: usize> (pub [[f64; N]; M]);
 
 impl<const M: usize, const N: usize> Matrix<M, N> {
     #[inline]
-    const fn zero() -> Self {
+    pub const fn zero() -> Self {
         Self([[0.0; N]; M])
     }
 
     #[inline]
-    const fn left_identity() -> Matrix<M, M> {
+    pub const fn left_identity() -> Matrix<M, M> {
         Matrix::<M, M>::identity()
     }
 
     #[inline]
-    const fn right_identity() -> Matrix<N, N> {
+    pub const fn right_identity() -> Matrix<N, N> {
         Matrix::<N, N>::identity()
+    }
+
+    pub const fn transpose(&self) -> Matrix<N, M> {
+        let mut result = Matrix::<N, M>::zero();
+        let mut i = 0;
+        let mut j;
+        while i < M {
+            j = 0;
+            while j < N {
+                result.0[j][i] = self.0[i][j];
+                j += 1;
+            }
+            i += 1;
+        }
+        result
+    }
+
+    pub const fn scale(&self, factor: f64) -> Matrix<M, N> {
+        let mut result = Matrix::<M, N>::zero();
+        let mut i = 0;
+        let mut j;
+        while i < M {
+            j = 0;
+            while j < N {
+                result.0[i][j] = self.0[i][j] * factor;
+                j += 1;
+            }
+            i += 1;
+        }
+        result
+    }
+
+    pub const fn scale_mut(&mut self, factor: f64) {
+        let mut i = 0;
+        let mut j;
+        while i < M {
+            j = 0;
+            while j < N {
+                self.0[i][j] = self.0[i][j] * factor;
+                j += 1;
+            }
+            i += 1;
+        }
     }
 }
 
 impl<const M: usize> Matrix<M, M> {
     #[inline]
-    const fn identity() -> Self {
+    pub const fn identity() -> Self {
         let mut result = Self::zero();
         let mut i = 0;
         while i < M {
